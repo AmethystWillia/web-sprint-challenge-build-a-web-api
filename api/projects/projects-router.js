da9d112a12ff68496bae99ea4f0dcf4b8033e1c6
Amethyst Willia <zmithic@gmail.com>
2022-02-11 13:08:53 -0600

    Added get request to get all projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
-// Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model');
+//const {} = require('./projects-middleware');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const res = require('express/lib/response');
 const server = express();
 
 server.use(express.json());
 
-const actionsRouter = require('./actions/actions-router');
+//const actionsRouter = require('./actions/actions-router');
 const projectsRouter = require('./projects/projects-router');
 
-res.send('<h2>Here we go again</h2>')
+server.use('/api/projects', projectsRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h2>Here we go again</h2>')
+});
 
 module.exports = server;
