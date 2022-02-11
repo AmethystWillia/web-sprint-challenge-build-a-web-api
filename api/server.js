c1905cb553a8421151f6b7e1357432d2304dd826
Amethyst Willia <zmithic@gmail.com>
2022-02-11 13:35:49 -0600

    Iimplemented get request to get all actions

--- api/server.js
+++ api/server.js
@@ -3,10 +3,11 @@ const server = express();
 
 server.use(express.json());
 
-//const actionsRouter = require('./actions/actions-router');
+const actionsRouter = require('./actions/actions-router');
 const projectsRouter = require('./projects/projects-router');
 
 server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
     res.send('<h2>Here we go again</h2>')
