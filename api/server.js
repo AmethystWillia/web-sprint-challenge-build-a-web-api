89eff4b8109764c84b820dec77a5a32fae696864
Amethyst Willia <zmithic@gmail.com>
2022-02-11 12:57:35 -0600

    Set up server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const res = require('express/lib/response');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+res.send('<h2>Here we go again</h2>')
 
 module.exports = server;
